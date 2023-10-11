02aab85dea873bc16d0094976220706cfa11c248
Alexis Sanchez <sanchezalexis500@gmail.com>
2023-10-11 13:59:09 -0400

    BasicCalculator update

--- BasicCalculator/src/main/java/BasicMath.java
+++ BasicCalculator/src/main/java/BasicMath.java
@@ -4,29 +4,40 @@ public class BasicMath {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the first number: ");
-        int num1 = scanner.nextInt();
+        double num1 = scanner.nextInt();
         System.out.print("Enter the second number: ");
-        int num2 = scanner.nextInt();
-        System.out.println("Possible calculations: \n" + "(A)dd\n" + "(S)ubtract\n" + "(M)ultiply\n" + "(D)ivide\n");
-        System.out.print("Please select an option: ");
-        String choice = scanner.next().toUpperCase();
-        int result = 0;
+        double num2 = scanner.nextInt();
 
-        switch (choice) {
-            case "A":
-                result = num1 + num2;
-                break;
-            case "S":
-                result = num1 - num2;
-                break;
-            case "M":
-                result = num1 * num2;
-                break;
-            case "D":
-                result = num1 / num2;
-                break;
-            default:
-               break;
+        System.out.print("Please select an option: ");
+
+        double result = 0;
+
+        boolean done = false;
+        while (!done) {
+            System.out.println("Possible calculations: \n" + "(A)dd\n" + "(S)ubtract\n" + "(M)ultiply\n" + "(D)ivide\n");
+            char choice = scanner.next().charAt(0);
+            char choiceUpper = Character.toUpperCase(choice);
+            switch (choiceUpper) {
+                case 'A':
+                    result = num1 + num2;
+                    done = true;
+                    break;
+                case 'S':
+                    result = num1 - num2;
+                    done = true;
+                    break;
+                case 'M':
+                    result = num1 * num2;
+                    done = true;
+                    break;
+                case 'D':
+                    result = num1 / num2;
+                    done = true;
+                    break;
+                default:
+                    System.out.println("Please enter a valid choice.");
+                    break;
+            }
         }
         System.out.println("Result: " + result);
     }
